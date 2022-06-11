4928e539c894cb2dc5db1efe1a615d0c87e6bea8
jadajada <65351417+Wiks0@users.noreply.github.com>
2022-06-11 20:21:14 +0200

    Create SpaceInvaders.java

---
+++ SpaceInvaders.java
@@ -0,0 +1,30 @@
+package proj.sprite;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+public class SpaceInvaders extends JFrame  {
+
+    public SpaceInvaders() {
+
+        zacetniUI();
+    }
+
+    private void zacetniUI() {
+
+        add(new Board());
+        setTitle("Space Invaders");
+        setSize(Nastavitve.sirinaOkna, Nastavitve.visinaOkna);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+        	var ex = new SpaceInvaders();
+            ex.setVisible(true);
+        });
+    }
+}
+    
