f6899b5c2398cc8d3ecff0e8e759913ac07e785c
jadajada <65351417+Wiks0@users.noreply.github.com>
2022-06-11 20:23:15 +0200

    Create Strel.java

---
+++ Strel.java
@@ -0,0 +1,23 @@
+package proj.sprite;
+
+import javax.swing.ImageIcon;
+
+public class Strel extends ObjektIgre {
+	public Strel() {}
+    public Strel(int x, int y) {
+    	initStrel(x, y);
+    }
+
+    private void initStrel(int x, int y) {
+
+        var strelIkona = "src/images/shot.png";
+        var ii = new ImageIcon(strelIkona);
+        setImage(ii.getImage());
+
+        int H_SPACE = 6;
+        setX(x + H_SPACE);
+
+        int V_SPACE = 1;
+        setY(y - V_SPACE);
+    }
+}
