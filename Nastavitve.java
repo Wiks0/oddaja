b5c67cb9fe325772145a19a5976b18116fd1f8d9
jadajada <65351417+Wiks0@users.noreply.github.com>
2022-06-11 20:22:27 +0200

    Create Nastavitve.java

---
+++ Nastavitve.java
@@ -0,0 +1,23 @@
+package proj.sprite;
+
+public interface Nastavitve {
+	//Splosne nastavitve 
+    int premikY = 16;
+    int steviloVesoljckov = 50;
+    int CHANCE = 5;
+    int DELAY = 17;
+    int PLAYER_WIDTH = 20;
+    int visinaIgralca = 20;
+	
+
+    int sirinaOkna = 600;
+    int visinaOkna = 700;
+    int BOMB_HEIGHT = 5;
+
+    int ALIEN_HEIGHT = 16;
+    int ALIEN_WIDTH = 16;
+    int ALIEN_INIT_X = 150;
+    int ALIEN_INIT_Y = 5;
+
+
+}
