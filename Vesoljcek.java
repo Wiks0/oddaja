7727efe059dcf976d7331b22899791f6176cf46b
jadajada <65351417+Wiks0@users.noreply.github.com>
2022-06-12 10:51:15 +0200

    Update Vesoljcek.java

--- Vesoljcek.java
+++ Vesoljcek.java
@@ -5,64 +5,54 @@ import javax.swing.ImageIcon;
 public class Vesoljcek extends ObjektIgre {
 
     private Bomba bomba;
-
     public Vesoljcek(int x, int y) {
 
-        initAlien(x, y);
+        initVesoljcek(x, y);
     }
-
-    private void initAlien(int x, int y) {
+    
+    private void initVesoljcek(int x, int y) {
 
         this.x = x;
         this.y = y;
 
         bomba = new Bomba(x, y);
-
-        var alienImg = "src/images/rsz_invadership.png";
-        var ii = new ImageIcon(alienImg);
-
-        setImage(ii.getImage());
     }
 
-    public void act(int smer) {
+    public void prestaviVesoljcka(int smer) {
 
         this.x += smer;
     }
 
-    public Bomba getBomb() {
+    public Bomba getBomba() {
 
         return bomba;
     }
 
     public class Bomba extends ObjektIgre {
 
-        private boolean destroyed;
-
+        private boolean unicen;
         public Bomba(int x, int y) {
-
-            initBomba(x, y);
+        	initBomba(x, y);
         }
 
         private void initBomba(int x, int y) {
-
-            setDestroyed(true);
-
-            this.x = x;
+        	setUnicen(true);
+        	this.x = x;
             this.y = y;
 
-            var bombImg = "src/images/bomb.png";
-            var ii = new ImageIcon(bombImg);
-            setImage(ii.getImage());
+            var bombaIkona = "src/images/bomb.png";
+            var bomba = new ImageIcon(bombaIkona);
+            setIkona(bomba.getImage());
         }
 
-        public void setDestroyed(boolean destroyed) {
+        public void setUnicen(boolean unicen) {
 
-            this.destroyed = destroyed;
+            this.unicen= unicen;
         }
 
-        public boolean isDestroyed() {
+        public boolean jeUnicen() {
 
-            return destroyed;
+            return unicen;
         }
     }
 }
